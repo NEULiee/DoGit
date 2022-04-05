4fd47b965468b0f685af52fa4006e70a7b86b5e5
neuli <asdfz888@naver.com>
2022-04-05 23:32:28 +0900

    first init

---
+++ DoGit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DoGit
+//
+//  Created by neuli on 2022/04/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
