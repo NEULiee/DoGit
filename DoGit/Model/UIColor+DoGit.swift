105f6299b5efd9070041029168fb1837b59eedc8
neuli <asdfz888@naver.com>
2022-04-05 23:53:59 +0900

    Color set 추가

---
+++ DoGit/Model/UIColor+DoGit.swift
@@ -0,0 +1,15 @@
+//
+//  UIColor+DoGit.swift
+//  DoGit
+//
+//  Created by neuli on 2022/04/05.
+//
+
+import UIKit
+
+extension UIColor {
+    
+    static var mainColor: UIColor {
+        UIColor(named: "mainColor") ?? .green
+    }
+}
