b6777723aeb2794f5f32ce0f62dbe5f0557fe7c7
neuli <asdfz888@naver.com>
2022-04-09 21:15:46 +0900

    확인 버튼 클릭 시 api 통신

--- DoGit/SetNameViewController/SetNameViewController+Actions.swift
+++ DoGit/SetNameViewController/SetNameViewController+Actions.swift
@@ -9,11 +9,26 @@ import UIKit
 
 extension SetNameViewController {
     
-    @objc func didChangeNameTextField(_ sender: UITextField) {
-        self.doneButton.isEnabled = sender.text == "" ? false : true
+    @objc func didPressDoneButton(_ sender: UIButton)  {
+        guard let name = self.nameTextField.text else { return }
+        userDataManager.fetchUser(userId: name) { result in
+            switch result {
+            case .success:
+                print("success")
+            case .failed:
+                DispatchQueue.main.sync {
+                    self.showAlert()
+                }
+            }
+        }
     }
     
-    @objc func didPressDoneButton(_ sender: UIButton) {
-
+    func showAlert() {
+        let alert = UIAlertController(title: "", message: "존재하지 않는 이름이에요.", preferredStyle: .alert)
+        alert.view.tintColor = .mainColor
+        alert.addAction(UIAlertAction(title: "다시입력", style: .default, handler: { [weak self] _ in
+            self?.dismiss(animated: true)
+        }))
+        present(alert, animated: true, completion: nil)
     }
 }
