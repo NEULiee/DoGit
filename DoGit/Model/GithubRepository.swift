95e4c6eaafd541b7cbeb8c2fee7ad87bb4e3445a
neuli <asdfz888@naver.com>
2022-04-22 18:34:17 +0900

    Codable 객체, collectionView 객체 분리

--- DoGit/Model/GithubRepository.swift
+++ DoGit/Model/GithubRepository.swift
@@ -1,25 +1,26 @@
 //
-//  GithubRepositoryList.swift
+//  GithubRepository.swift
 //  DoGit
 //
-//  Created by neuli on 2022/04/16.
+//  Created by neuli on 2022/04/22.
 //
 
 import Foundation
 
-struct GithubRepository: Codable, Hashable, Identifiable {
+struct GithubRepository: Hashable, Identifiable {
     let id: Int64
     let name: String
-    let description: String?
-    
-    init(_ id: Int64, _ name: String) {
-        self.init(id, name, "")
-    }
+    let description: String
+    var isCheck: Bool = false
     
     init(_ id: Int64, _ name: String, _ description: String) {
         self.id = id
         self.name = name
         self.description = description
+    }
+    
+    mutating func setCheck() {
+        self.isCheck.toggle()
     }
     
     func contains(_ filter: String?) -> Bool {
