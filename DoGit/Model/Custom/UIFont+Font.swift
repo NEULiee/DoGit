51f144e4f47cd3b35a0862c7d66f213703db37f9
neuli <asdfz888@naver.com>
2022-04-27 00:32:08 +0900

    기본폰트로 변경

--- DoGit/Model/Custom/UIFont+Font.swift
+++ DoGit/Model/Custom/UIFont+Font.swift
@@ -7,36 +7,34 @@
 
 import UIKit
 
-    // UIFont(name: Font.NEXONLv1GothicLowOTFRegular.rawValue, size: 18)
-
 extension UIFont {
     
     class Font {
-        static let regular8: UIFont = UIFont(name: "NEXONLv1GothicLowOTFRegular", size: 8) ?? UIFont.systemFont(ofSize: 8)
-        static let regular10: UIFont = UIFont(name: "NEXONLv1GothicLowOTFRegular", size: 10) ?? UIFont.systemFont(ofSize: 10)
-        static let regular12: UIFont = UIFont(name: "NEXONLv1GothicLowOTFRegular", size: 12) ?? UIFont.systemFont(ofSize: 12)
-        static let regular14: UIFont = UIFont(name: "NEXONLv1GothicLowOTFRegular", size: 14) ?? UIFont.systemFont(ofSize: 14)
-        static let regular16: UIFont = UIFont(name: "NEXONLv1GothicLowOTFRegular", size: 16) ?? UIFont.systemFont(ofSize: 16)
-        static let regular18: UIFont = UIFont(name: "NEXONLv1GothicLowOTFRegular", size: 18) ?? UIFont.systemFont(ofSize: 18)
-        static let regular24: UIFont = UIFont(name: "NEXONLv1GothicLowOTFRegular", size: 24) ?? UIFont.systemFont(ofSize: 24)
-        static let regular32: UIFont = UIFont(name: "NEXONLv1GothicLowOTFRegular", size: 32) ?? UIFont.systemFont(ofSize: 32)
+        static let regular8: UIFont = UIFont(name: "NEXONLv2GothicOTF", size: 8) ?? UIFont.systemFont(ofSize: 8)
+        static let regular10: UIFont = UIFont(name: "NEXONLv2GothicOTF", size: 10) ?? UIFont.systemFont(ofSize: 10)
+        static let regular12: UIFont = UIFont(name: "NEXONLv2GothicOTF", size: 12) ?? UIFont.systemFont(ofSize: 12)
+        static let regular14: UIFont = UIFont(name: "NEXONLv2GothicOTF", size: 14) ?? UIFont.systemFont(ofSize: 14)
+        static let regular16: UIFont = UIFont(name: "NEXONLv2GothicOTF", size: 16) ?? UIFont.systemFont(ofSize: 16)
+        static let regular18: UIFont = UIFont(name: "NEXONLv2GothicOTF", size: 18) ?? UIFont.systemFont(ofSize: 18)
+        static let regular24: UIFont = UIFont(name: "NEXONLv2GothicOTF", size: 24) ?? UIFont.systemFont(ofSize: 24)
+        static let regular32: UIFont = UIFont(name: "NEXONLv2GothicOTF", size: 32) ?? UIFont.systemFont(ofSize: 32)
         
-        static let light8: UIFont = UIFont(name: "NEXONLv1GothicLowOTFLight", size: 8) ?? UIFont.systemFont(ofSize: 8)
-        static let light10: UIFont = UIFont(name: "NEXONLv1GothicLowOTFLight", size: 10) ?? UIFont.systemFont(ofSize: 10)
-        static let light12: UIFont = UIFont(name: "NEXONLv1GothicLowOTFLight", size: 12) ?? UIFont.systemFont(ofSize: 12)
-        static let light14: UIFont = UIFont(name: "NEXONLv1GothicLowOTFLight", size: 14) ?? UIFont.systemFont(ofSize: 14)
-        static let light16: UIFont = UIFont(name: "NEXONLv1GothicLowOTFLight", size: 16) ?? UIFont.systemFont(ofSize: 16)
-        static let light18: UIFont = UIFont(name: "NEXONLv1GothicLowOTFLight", size: 18) ?? UIFont.systemFont(ofSize: 18)
-        static let light24: UIFont = UIFont(name: "NEXONLv1GothicLowOTFLight", size: 24) ?? UIFont.systemFont(ofSize: 24)
-        static let light32: UIFont = UIFont(name: "NEXONLv1GothicLowOTFLight", size: 32) ?? UIFont.systemFont(ofSize: 32)
+        static let light8: UIFont = UIFont(name: "NEXONLv2GothicOTFLight", size: 8) ?? UIFont.systemFont(ofSize: 8, weight: .light)
+        static let light10: UIFont = UIFont(name: "NEXONLv2GothicOTFLight", size: 10) ?? UIFont.systemFont(ofSize: 10, weight: .light)
+        static let light12: UIFont = UIFont(name: "NEXONLv2GothicOTFLight", size: 12) ?? UIFont.systemFont(ofSize: 12, weight: .light)
+        static let light14: UIFont = UIFont(name: "NEXONLv2GothicOTFLight", size: 14) ?? UIFont.systemFont(ofSize: 14, weight: .light)
+        static let light16: UIFont = UIFont(name: "NEXONLv2GothicOTFLight", size: 16) ?? UIFont.systemFont(ofSize: 16, weight: .light)
+        static let light18: UIFont = UIFont(name: "NEXONLv2GothicOTFLight", size: 18) ?? UIFont.systemFont(ofSize: 18, weight: .light)
+        static let light24: UIFont = UIFont(name: "NEXONLv2GothicOTFLight", size: 24) ?? UIFont.systemFont(ofSize: 24, weight: .light)
+        static let light32: UIFont = UIFont(name: "NEXONLv2GothicOTFLight", size: 32) ?? UIFont.systemFont(ofSize: 32, weight: .light)
         
-        static let bold8: UIFont = UIFont(name: "NEXONLv1GothicLowOTFBold", size: 8) ?? UIFont.systemFont(ofSize: 8)
-        static let bold10: UIFont = UIFont(name: "NEXONLv1GothicLowOTFBold", size: 10) ?? UIFont.systemFont(ofSize: 10)
-        static let bold12: UIFont = UIFont(name: "NEXONLv1GothicLowOTFBold", size: 12) ?? UIFont.systemFont(ofSize: 12)
-        static let bold14: UIFont = UIFont(name: "NEXONLv1GothicLowOTFBold", size: 14) ?? UIFont.systemFont(ofSize: 14)
-        static let bold16: UIFont = UIFont(name: "NEXONLv1GothicLowOTFBold", size: 16) ?? UIFont.systemFont(ofSize: 16)
-        static let bold18: UIFont = UIFont(name: "NEXONLv1GothicLowOTFBold", size: 18) ?? UIFont.systemFont(ofSize: 18)
-        static let bold24: UIFont = UIFont(name: "NEXONLv1GothicLowOTFBold", size: 24) ?? UIFont.systemFont(ofSize: 24)
-        static let bold32: UIFont = UIFont(name: "NEXONLv1GothicLowOTFBold", size: 32) ?? UIFont.systemFont(ofSize: 32)
+        static let bold8: UIFont = UIFont(name: "NEXONLv2GothicOTFMedium", size: 8) ?? UIFont.systemFont(ofSize: 8, weight: .bold)
+        static let bold10: UIFont = UIFont(name: "NEXONLv2GothicOTFMedium", size: 10) ?? UIFont.systemFont(ofSize: 10, weight: .bold)
+        static let bold12: UIFont = UIFont(name: "NEXONLv2GothicOTFMedium", size: 12) ?? UIFont.systemFont(ofSize: 12, weight: .bold)
+        static let bold14: UIFont = UIFont(name: "NEXONLv2GothicOTFMedium", size: 14) ?? UIFont.systemFont(ofSize: 14, weight: .bold)
+        static let bold16: UIFont = UIFont(name: "NEXONLv2GothicOTFMedium", size: 16) ?? UIFont.systemFont(ofSize: 16, weight: .bold)
+        static let bold18: UIFont = UIFont(name: "NEXONLv2GothicOTFMedium", size: 18) ?? UIFont.systemFont(ofSize: 18, weight: .bold)
+        static let bold24: UIFont = UIFont(name: "NEXONLv2GothicOTFMedium", size: 24) ?? UIFont.systemFont(ofSize: 24, weight: .bold)
+        static let bold32: UIFont = UIFont(name: "NEXONLv2GothicOTFMedium", size: 32) ?? UIFont.systemFont(ofSize: 32, weight: .bold)
     }
 }
