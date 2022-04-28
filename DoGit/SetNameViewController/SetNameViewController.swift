aada41a5262dd5cc813f727a15ed95f4ccfad435
neuli <asdfz888@naver.com>
2022-04-28 22:52:33 +0900

    UIAlertController completion 삭제

--- DoGit/SetNameViewController/SetNameViewController.swift
+++ DoGit/SetNameViewController/SetNameViewController.swift
@@ -31,9 +31,7 @@ class SetNameViewController: UIViewController {
         let alert = UIAlertController(title: "", message: errorDescription, preferredStyle: .alert)
         alert.view.accessibilityIdentifier = "nameAlert"
         alert.view.tintColor = .mainColor
-        alert.addAction(UIAlertAction(title: "다시입력", style: .default, handler: { [weak self] _ in
-            self?.dismiss(animated: true)
-        }))
+        alert.addAction(UIAlertAction(title: "다시입력", style: .default, handler: nil))
         present(alert, animated: true, completion: nil)
     }
 }
