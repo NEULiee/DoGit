267b224ba4d204452f08106ca7e5a5669187307c
neuli <asdfz888@naver.com>
2022-04-29 17:05:28 +0900

    UI 수정

--- DoGit/Model/Custom/UITextField+UnderLine.swift
+++ DoGit/Model/Custom/UITextField+UnderLine.swift
@@ -21,7 +21,7 @@ extension UITextField {
     func underLine(borderColor: UIColor) {
         
         let border = CALayer()
-        border.frame = CGRect(x: 0, y: self.frame.size.height + 2, width: self.frame.width, height: 1)
+        border.frame = CGRect(x: 0, y: self.frame.size.height + 2, width: self.frame.width - 32, height: 1)
         border.borderWidth = 2
         border.borderColor = borderColor.cgColor
         self.layer.addSublayer(border)
